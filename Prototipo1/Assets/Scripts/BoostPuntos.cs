e92fd88682effd711cd2aff8e0b625c5e0567647
Luis Angel Leyva Castillo <luis_angel_howke@ciencias.unam.mx>
2023-09-24 19:19:43 -0600

    Update BoostPuntos.cs

--- Prototipo1/Assets/Scripts/BoostPuntos.cs
+++ Prototipo1/Assets/Scripts/BoostPuntos.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoosPuntos : MonoBehaviour
+public class BoostPuntos : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

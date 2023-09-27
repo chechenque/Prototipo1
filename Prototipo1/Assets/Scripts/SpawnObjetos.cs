01f8e84af7f799fb7acf09389f86b15d9036a560
Luis Angel Leyva Castillo <luis_angel_howke@ciencias.unam.mx>
2023-09-27 11:04:10 -0600

    Spawn Items

--- Prototipo1/Assets/Scripts/SpawnObjetos.cs
+++ Prototipo1/Assets/Scripts/SpawnObjetos.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class SpawnObjetos : MonoBehaviour{
     public GameObject[] spawns = new GameObject[7];
-    public GameObject[] cars = new GameObject[3];
+    public GameObject[] cars = new GameObject[4];
     public GameObject[] powerUp = new GameObject[1];
-    public GameObject[] obstaculos = new GameObject[2];
+    public GameObject[] obstaculos = new GameObject[3];
 
     int s, c;
 
     private void Start() {
-        for(int i = 0; i < cars.Length; ++i) {
-            s = Random.Range(0, 4);
+        for(int i = 0; i < 3; ++i) {
+            s = Random.Range(0, 7);
             c = Random.Range(0, 4);
+            Instantiate(cars[c], spawns[s].transform.position, spawns[s].transform.rotation);
         }
+
+        for(int i = 0; i < 3; ++i) {
+            s = Random.Range(0, 7);
+            c = Random.Range(0, 3);
+            Instantiate(obstaculos[c], spawns[s].transform.position, spawns[s].transform.rotation);
+        }
+
+        s = Random.Range(0, 7);
+        c = Random.Range(0, 2);
+        Instantiate(powerUp[c], spawns[s].transform.position, spawns[s].transform.rotation);
     }
 }

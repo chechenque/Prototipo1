d3e070ed12657ee4aaa23d9c48fee168201815a3
Luis Angel Leyva Castillo <luis_angel_howke@ciencias.unam.mx>
2023-09-29 19:10:40 -0600

    FixedLuz

--- Prototipo1/Assets/Scripts/Luz.cs
+++ Prototipo1/Assets/Scripts/Luz.cs
@@ -7,12 +7,12 @@ public class Luz : MonoBehaviour{
 
     // Start is called before the first frame update
     void Start() {
-        transform.Rotate(75f, 0, 0);
+        transform.Rotate(50f, 0, 0);
     }
 
     // Update is called once per frame
     void Update() {
-        if (this.transform.rotation.eulerAngles.x > 80) {
+        if (this.transform.rotation.eulerAngles.x > 60) {
             transform.Rotate(-6f, 0, 0);
         } else {
             transform.Rotate(speed * Time.deltaTime, 0, 0);
